b278b88491e1d0ecc88552ce5b36a9a2d718024d
davisoutoneri <davisouto.dev@gmail.com>
2022-09-14 20:57:17 -0300

    Aula 25-27: Inicializando o projeto. Aula: 27-29: Desenvolvendo o Frot, Header e Footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './components/template/header/header.component';
+import { AllSharedMaterialsModule } from './sharing/all-materials/all-shared-materials/all-shared-materials.module';
+import { FooterComponent } from './components/template/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    AllSharedMaterialsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
