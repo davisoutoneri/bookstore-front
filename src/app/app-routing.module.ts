45c55dbcf823f44fb88be8842406d1108192cecf
davisoutoneri <davisouto.dev@gmail.com>
2022-09-20 19:00:12 -0300

    Aula 53: Implementação do Componente LIVRO DELETE

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { LivroDeleteComponent } from './components/views/livro/livro-delete/livro-delete.component';
 import { LivroUpdateComponent } from './components/views/livro/livro-update/livro-update.component';
 import { LivroReadAllComponent } from './components/views/livro/livro-read-all/livro-read-all.component';
 import { CategoriaUpdateComponent } from './components/views/categoria/categoria-update/categoria-update.component';
@@ -41,6 +42,10 @@ const routes: Routes = [
   {
     path: 'categorias/:id_cat/livros/:id/update',
     component: LivroUpdateComponent
+  },
+  {
+    path: 'categorias/:id_cat/livros/:id/delete',
+    component: LivroDeleteComponent
   }
 ];
 
---
+++ src/app/components/views/livro/livro-delete/livro-delete.component.ts
@@ -0,0 +1,59 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+
+import { Livro } from '../livro.model';
+import { LivroService } from '../livro.service';
+
+@Component({
+  selector: 'app-livro-delete',
+  templateUrl: './livro-delete.component.html',
+  styleUrls: ['./livro-delete.component.css']
+})
+export class LivroDeleteComponent implements OnInit {
+
+  id_categoria: String = ''
+
+  livro: Livro = {
+    id: '',
+    titulo: '',
+    nome_autor: '',
+    texto: ''
+  }
+
+  constructor(
+    private service: LivroService,
+    private route: ActivatedRoute,
+    private router: Router  
+  ) { }
+
+  ngOnInit(): void {
+    this.id_categoria = this.route.snapshot.paramMap.get('id_cat')!;
+    this.livro.id = this.route.snapshot.paramMap.get('id')!;
+    this.findById();
+  }
+
+  findById(): void{
+    this.service.findById(this.livro.id!).subscribe((resposta) => {
+      this.livro = resposta
+    });
+  }
+
+  delete():void {
+    this.service.delete(this.livro.id!).subscribe(() => {
+      this.router.navigate(['categorias/'+`${this.id_categoria}`+'/livros'])
+      this.service.mensagem('Livro deletado com sucesso!')
+    }, err => {
+      this.service.mensagem('Falha ao deletar livro! Tente mais tarde!')
+    })
+  }
+
+  getMessage() {
+    
+    return false;
+  }
+
+  cancel(){
+    this.router.navigate(['categorias/'+`${this.id_categoria}`+'/livros'])
+  }
+
+}
